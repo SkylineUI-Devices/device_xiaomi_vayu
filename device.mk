02effee1bdd002dfce8161735f9295005c697f0d
GXC2356 <gxcdev2356@gmail.com>
2024-01-05 13:14:15 +0200

    Revert: vayu: Add prebuilt dolby in settings

--- device.mk
+++ device.mk
@@ -512,10 +512,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Dolby
-PRODUCT_PACKAGES += \
-    XiaomiDolby
-
 # Inherit from ViPER4AndroidFX
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
