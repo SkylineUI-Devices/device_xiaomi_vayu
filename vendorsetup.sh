d683dcc4ee301b3da52d682c03c936548434af32
GXC2356 <gxcdev2356@gmail.com>
2024-06-04 16:25:30 +0300

    Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Vendor
+git clone --depth=1 https://github.com/ProjectBlaze-Devices/vendor_xiaomi_vayu.git vendor/xiaomi/vayu
+
+# Kernel (Skyline)
+git clone --depth=1 https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/vayu
