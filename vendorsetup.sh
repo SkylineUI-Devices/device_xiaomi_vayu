5f32269a4feaef687008520334ef73df300a2792
GXC2356 <gxcdev2356@gmail.com>
2024-08-02 16:15:45 +0000

    vayu: vendorsetup.sh: Clone hardware_xiaomi repo from LineageOS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,7 @@ git clone --depth=1 https://github.com/SkylineUI-Devices/vendor_xiaomi_vayu.git 
 # Kernel (Skyline)
 rm -rf kernel/xiaomi/vayu
 git clone --depth=1 https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/vayu
+
+# Hardware
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
