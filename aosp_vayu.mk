2c778e11493200b21de282998e4999cce8390692
GXC2356 <gxcdev2356@gmail.com>
2024-11-19 06:41:08 +0200

    vayu: Return back eppe disable

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -15,6 +15,7 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
+TARGET_DISABLE_EPPE := true
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
