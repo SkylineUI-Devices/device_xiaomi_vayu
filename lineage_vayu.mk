7d5c881d86473ba5f8eaa15932320f0dd23c78c9
Yordan A. <106074709+Dachoni03@users.noreply.github.com>
2024-12-30 13:22:10 +0200

    Add LOS 22.1 support

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2021 Android Open Source Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,30 +12,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common Aosp configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_DISABLE_EPPE := true
-
-# SkylineUI Maintainer Flags
-SKYLINEUI_MAINTAINER := GXC2356
-CUSTOM_BUILD_TYPE := OFFICIAL
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 13 TKQ1.221013.002 V14.0.1.0.TJUMIXM release-keys" \
-    PRODUCT_NAME=vayu_global \
-    PRODUCT_MODEL=M2102J20SG
-
-BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:13/TKQ1.221013.002/V14.0.1.0.TJUMIXM:user/release-keys
